1073ab7118124e8341875d45bed508975610a971
suwampy <tndhks123@nate.com>
2023-04-09 15:36:12 +0900

    5장 Customer, Resevation 구현

--- src/ch05/Customer.java
+++ src/ch05/Customer.java
@@ -1,2 +1,11 @@
-package ch05;public class Customer {
+package ch05;
+
+public class Customer {
+    private String name;
+    private String id;
+
+    public Customer(String name, String id) {
+        this.id = id;
+        this.name = name;
+    }
 }
--- src/ch05/Reservation.java
+++ src/ch05/Reservation.java
@@ -1,2 +1,15 @@
-package ch05;public class Reservation {
+package ch05;
+
+public class Reservation {
+    private Customer customer;
+    private Screening screening;
+    private Money fee;
+    private int audienceCount;
+
+    public Reservation(Customer customer, Screening screening, Money fee, int audienceCount) {
+        this.customer = customer;
+        this.screening = screening;
+        this.fee = fee;
+        this.audienceCount = audienceCount;
+    }
 }
